7ef5716f67a5c1a8489701d97deb3658cefef661
mariosrb <mario-2000@hotmail.fr>
2025-03-21 12:50:53 +0100

    entraienemnt

--- train.c
+++ train.c
@@ -4,19 +4,15 @@
 #include <unistd.h>
 
 int mails = 0;
-int	lock = 0;
-
+pthread_mutex_t	mutex;
+// Mutex = protger pour empecher lautre thread de toucher aux variables partages
 void *routine_thread(void *arg)
 {
 	for (int i = 0; i < 200000; i++)
 	{
-		if (lock == 1)
-		{
-			// wait until lock is 0
-		}
-		lock = 1;
+		pthread_mutex_lock(&mutex);
 		mails++;
-		lock = 0;
+		pthread_mutex_unlock(&mutex);
 	}
 	return (NULL);
 }
@@ -24,11 +20,13 @@ void *routine_thread(void *arg)
 int main(int ac, char **av)
 {
 	pthread_t p1, p2;
+	pthread_mutex_init(&mutex, NULL);
 	pthread_create(&p1, NULL, &routine_thread, NULL);
 	pthread_create(&p2, NULL, &routine_thread, NULL);
 	
 	pthread_join(p1, NULL);
 	pthread_join(p2, NULL);
+	pthread_mutex_destroy(&mutex);
 	printf("Number of mails %d\n", mails);
     return (0);
 }
