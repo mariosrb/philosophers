9ad12ec866c435e8b5199808f903a27069b0da5e
mariosrb <mario-2000@hotmail.fr>
2025-03-28 15:05:56 +0100

    essaie

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: mdodevsk <mdodevsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 12:38:11 by mdodevsk          #+#    #+#             */
-/*   Updated: 2025/03/28 12:54:50 by mdodevsk         ###   ########.fr       */
+/*   Updated: 2025/03/28 15:02:26 by mdodevsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,12 @@
 void *philosopher_routine(void *arg)
 {
 	t_philo	*philo;
+	int		i;
 
+	i = 0;
 	philo = (t_philo *)arg;
+	if (philo->id % 2 == 0)
+		usleep(1000);
 	return (NULL);
+	philo->last_meal = get_current_time();
 }
